06d6c82a3fb06d7c6f1eaa44e8f3f36685993fae
vitalik-fan <63237530+vitalik-fan@users.noreply.github.com>
2022-02-02 11:24:13 +0300

    сделали меню слева

--- shop-frontend/pages/_app.tsx
+++ shop-frontend/pages/_app.tsx
@@ -1,4 +1,12 @@
 import '../styles/globals.css'
+import '../styles/main.css'
+
+// import '../styles/bootstrap.min.css'
+// import '../styles/classy-nav.min.css'
+// import '../styles/core-style.css'
+
+import '../scss/style.scss'
+
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
--- shop-frontend/pages/index.tsx
+++ shop-frontend/pages/index.tsx
@@ -1,7 +1,11 @@
+import Head from '../hoc/Head'
+
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2 text-blue-500">
-      hello
-    </div>
+    <Head>
+      <div className="flex min-h-screen flex-col items-center justify-center py-2 text-blue-500">
+        hello
+      </div>
+    </Head>
   )
 }
